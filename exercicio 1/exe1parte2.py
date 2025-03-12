664ab35742f489c947012fb4f0249f967a98fe8e
PietroSMoliani <pietromoliani@colegiopaulodetarso.com.br>
2025-03-12 21:17:15 +0000

    atividade 6 python

---
+++ exercicio 1/exe1parte2.py
@@ -0,0 +1,12 @@
+numero = input("Digite um número para somarmos seus dígitos: ")
+
+
+if numero.isdigit():
+    soma = 0
+
+    for digito in numero:
+        soma += int(digito)
+    print("A soma dos dígitos é:", soma)
+else:
+    print("Erro você precisa digitar um número inteiro.")
+
